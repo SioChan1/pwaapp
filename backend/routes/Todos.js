b8b0057ac5b9beb5e642ec2e731dff86aa86286d
SioChan1 <palivonaite95@gmail.com>
2023-05-16 13:21:17 +0200

    Initial routes

---
+++ backend/routes/Todos.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const Todo = require('../models/Todos')
+
+// Get all Todo route
+router.get('/', async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos)
+})
+
+// create new Todo
+router.post('/new', async (res, req) => {
+    const newTodo = new Todo(
+        //req.body // what vue app is sending 
+        {
+            author: "Fox",
+            todo: "hunt"
+        }
+    );
+    const saveTodo = await newTodo.save()
+    res.json(saveTodo)
+})
+
+// getter by id 
+router.get('/get/:id', async (res, req) => {
+    const t = await Todo.findById({ _id : req.params.id })
+    res.json(t)
+})
+
+// delete todo by id
+router.delete('/delete/:id', async (res, req) => {
+    const tDelete = await Todo.findByIdAndDelete({ _id : req.params.id })
+    res.json(tDelete)
+})
+
+// update todo by id
+router.put('/update/:id', async (res, req) => {
+    const tUpdate = await Todo.updateOne(
+        // { _id: req.params.id }, { $set: req.body }
+        {
+            author: "Cat",
+            todo: "sleep"
+        }
+    )
+    res.json(tUpdate)
+})
+
+module.exports = router 
