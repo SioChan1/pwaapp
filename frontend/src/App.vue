9df5c0e3d3b086512ab3288be583fcc8860fdb77
SioChan1 <palivonaite95@gmail.com>
2023-05-17 11:21:02 +0200

    ToDos crud start

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,6 +11,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/todos">Todos</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
