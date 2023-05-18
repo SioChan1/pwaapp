cf2260ee5e38603f32c62f89ca0f0e4ad8edd498
SioChan1 <palivonaite95@gmail.com>
2023-05-18 13:35:10 +0200

    Looked for error that did not show the list when in edit tab

--- frontend/src/views/TodoDetail.vue
+++ frontend/src/views/TodoDetail.vue
@@ -1,41 +1,46 @@
 <template>
     <div>
-        <h1>
-            Editing a Todo
-        </h1>
-        <div v-for="item in todo" :key="item._id">
-            Author: <h4>{{ item.author }}</h4>
-            Todo: <p>{{ item.todo }}</p>
-        </div>
-        <hr>
-        <input type="text" placeholder="Author" v-model="state.newAuthor">
+      <h1>Editing a ToDo</h1>
+  
+      <div v-for="item in todo" :key="item._id">
+        Author: <h4>{{item.author}}</h4>
+        Todo Item: <p>{{item.todo}}</p>
+      </div>
+  
+      <hr>
+  
+      <input type="text" placeholder="Author" v-model="state.newAuthor">
       <span> Test: {{ state.newAuthor }} </span>
       <br>
       <input type="text" placeholder="Todo" v-model="state.newTodoItem">
       <span> Test: {{ state.newTodoItem }} </span>
       <br>
-      <button @click="editTodo">Update</button>
-      
+      <button @click="editTodo()">Update Item</button>
+  
       <hr>
     </div>
-</template>
-
-<script>
-import todocrud from '../modules/todocrud'
+  </template>
+  
+  <script>
+  import todocrud from '../modules/todocrud'
+  
     export default {
-        setup() {
-
-            const { editTodo, state } = todocrud()
-
-            return {
-                editTodo,
-                state
-            }
-
+      setup() {
+  
+        const { editTodo, state, GetSpecificTodo, todo, todoId } = todocrud()
+        
+        GetSpecificTodo()
+  
+        return {
+          todo,
+          todoId,
+          editTodo,
+          state
         }
+      }
     }
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style>
